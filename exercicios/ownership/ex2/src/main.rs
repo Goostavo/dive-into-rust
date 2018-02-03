79821c9f6112838ffbd876dac3a68f26dd2c7b63
Gustavo Garcia <goostavobg@gmail.com>
2018-02-03 17:41:23 -0200

    Ownership exercises 1 n 2

--- exercicios/ownership/ex2/src/main.rs
+++ exercicios/ownership/ex2/src/main.rs
@@ -2,10 +2,10 @@
 // O compilador tem razão em reclamar do seu código?
 fn main() {
     let mut vetor = vec![1, 3, 2];
-    let mut maior = &vetor[0];
+    let mut maior = vetor[0];
     for x in &vetor {
-        if x > maior {
-            maior = x;
+        if *x > maior {
+            maior = *x;
         }
     }
     vetor.push(maior + 1);
