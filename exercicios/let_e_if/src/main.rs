98a0bad559214572a726389a13affc86e21c5e3e
Gustavo Garcia <goostavobg@gmail.com>
2018-02-03 14:50:42 -0200

    Created function for let_e_if

--- exercicios/let_e_if/src/main.rs
+++ exercicios/let_e_if/src/main.rs
@@ -6,6 +6,9 @@ fn main() {
 /// exceto em anos múltiplos de 100 que não são múltiplos de 400.
 /// Retorne `true` se `year` é bissexto e `false` caso contrário.
 fn is_leap_year(year: i32) -> bool {
+    if year % 4 == 0 && !(year % 100 == 0 && year % 400 != 0) {
+        return true;
+    }
     false
 }
 
